748d9d2718ad3166f70bc1695df91438b0a853fb
Phuc Lam <phuclamtran2810@gmail.com>
2025-12-22 10:26:56 +0700

    PART1: Thiết kế bảng tokens, social_accounts, categories, products

--- SHOPAPP-BACKEND/database.sql
+++ SHOPAPP-BACKEND/database.sql
@@ -18,3 +18,45 @@ CREATE TABLE users(
 );
 -- Lưu ý:
 -- "is_active" để tinint vì để boolean khi đổi cơ sở dữ liệu khác đôi khi sẽ không hỗ trợ boolean
+
+CREATE TABLE tokens(
+    id INT PRiMARY KEY AUTO_INCREMENT,
+    token VARCHAR(255) UNIQUE NOT NULL,
+    token_type VARCHAR(50) NOT NULL,
+    expiration_date DATETIME,
+    revoked TINYINT(1) NOT NULL,
+    expired TINYINT(1) NOT NULL,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+-- hỗ trợ đăng nhập từ Facebook và Google
+CREATE TABLE social_accounts(
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `provider` VARCHAR(50) NOT NULL COMMENT 'e.g., facebook, google',
+    `provider__id` VARCHAR(50) NOT NULL,
+    `email` VARCHAR(150) NOT NULL COMMENT 'Email tài khoản xã hội',
+    `name` VARCHAR(100) NOT NULL COMMENT 'Tên người dùng',
+    `user_id` INT,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+--Bảng danh mục sản phẩm(Categories)
+CREATE TABLE categories(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL DEFAULT "" COMMENT 'Tên danh mục, vd: Điện thoại, Laptop'
+);
+
+--Bảng chứa san phẩm(Products): "laptop macbook pro 2023", "iphone 14 pro max",...
+
+CREATE TABLE products(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(350) NOT NULL DEFAULT "" COMMENT 'Tên sản phẩm',
+    price FLOAT NOT NULL CHECK (price >= 0),
+    thumbnail VARCHAR(300) DEFAULT "",
+    description LONGTEXT DEFAULT "",
+    created_at DATETIME,
+    updated_at DATETIME,
+    category_id INT,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
