8cd0b6df74156da3bac5d71920a9c314259d1be7
Phuc Lam <phuclamtran2810@gmail.com>
2026-01-15 15:52:14 +0700

    PART4: Thêm dữ liệu fake vào bảng product

--- SHOPAPP-BACKEND/shopapp/src/main/java/com/project/shopapp/controllers/ProductController.java
+++ SHOPAPP-BACKEND/shopapp/src/main/java/com/project/shopapp/controllers/ProductController.java
@@ -1,5 +1,6 @@
 package com.project.shopapp.controllers;
 
+import com.github.javafaker.Faker;
 import com.project.shopapp.dtos.ProductDTO;
 import com.project.shopapp.dtos.ProductImageDTO;
 import com.project.shopapp.models.Product;
@@ -157,5 +158,29 @@ public class ProductController {
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteProductB(@PathVariable("id") long id) {
         return ResponseEntity.status(HttpStatus.OK).body("Delete successfully product with id: " + id);
+    }
+
+    @PostMapping("/generateFakeProducts")
+    public ResponseEntity<String> generateFakeProducts() {
+        Faker faker = new Faker();
+        for (int i = 0; i < 10_000; i++) {
+            String productName = faker.commerce().productName();
+            if (productService.existsByName(productName)) {
+                continue;
+            }
+            ProductDTO productDTO = ProductDTO.builder()
+                    .name(productName)
+                    .price((float) faker.number().numberBetween(10, 90_000_000))
+                    .description(faker.lorem().sentence())
+                    .thumbnail("")
+                    .categoryId((long) faker.number().numberBetween(2, 5))
+                    .build();
+            try {
+                productService.createProduct(productDTO);
+            } catch (Exception e) {
+                return ResponseEntity.badRequest().body(e.getMessage());
+            }
+        }
+        return ResponseEntity.ok("Fake products generated successfully");
     }
 }
