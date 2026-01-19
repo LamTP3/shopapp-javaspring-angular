e9c9495c89effa4610da6efa1a7ce7bd36624360
Phuc Lam <phuclamtran2810@gmail.com>
2026-01-19 15:16:52 +0700

    PART5:  Tùy biến các hàm Jwt, Login, Register User

--- SHOPAPP-BACKEND/shopapp/src/main/java/com/project/shopapp/services/UserService.java
+++ SHOPAPP-BACKEND/shopapp/src/main/java/com/project/shopapp/services/UserService.java
@@ -1,5 +1,6 @@
 package com.project.shopapp.services;
 
+import com.project.shopapp.components.JwtTokenUtil;
 import com.project.shopapp.dtos.UserDTO;
 import com.project.shopapp.exceptions.DataNotFoundException;
 import com.project.shopapp.models.Role;
@@ -8,13 +9,22 @@ import com.project.shopapp.repositories.RoleRepository;
 import com.project.shopapp.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class UserService implements IUserService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenUtil jwtTokenUtil;
+    private final AuthenticationManager authenticationManager;
 
     @Override
     public User createUser(UserDTO userDTO) throws DataNotFoundException {
@@ -41,17 +51,34 @@ public class UserService implements IUserService {
         // Kiểm tra nếu có accountId, không yêu cầu mật khẩu
         if (newUser.getFacebookAccountId() == 0 && newUser.getGoogleAccountId() == 0) {
             String password = userDTO.getPassword();
-            // String encodedPassword = passwordEncoder.encode(password); // Mã hóa mật khẩu ở đây
-            // newUser.setPassword(encodedPassword);
-
+            String encodedPassword = passwordEncoder.encode(password);
+            newUser.setPassword(encodedPassword);
         }
         newUser.setPassword(null);
         return userRepository.save(newUser);
     }
 
     @Override
-    public String login(String phoneNumber, String password) {
-
-        return null;
+    public String login(String phoneNumber, String password) throws Exception {
+        Optional<User> optionalUser = userRepository.findByPhoneNumber(phoneNumber);
+        if (optionalUser.isEmpty()) {
+            throw new DataNotFoundException("Invalid phone number or password");
+        }
+        // muốn trả về JWT token thì phải làm thêm
+        User existingUser = optionalUser.get();
+        // check password
+        if (existingUser.getFacebookAccountId() == 0
+                && existingUser.getGoogleAccountId() == 0) {
+            if (!passwordEncoder.matches(password, existingUser.getPassword())) {
+                throw new BadCredentialsException("Wrong phone number or password");
+            }
+        }
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                phoneNumber,
+                password
+        );
+        //authenticate with Java Spring Security
+        authenticationManager.authenticate(authenticationToken);
+        return jwtTokenUtil.generateToken(existingUser);
     }
 }
